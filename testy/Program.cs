7f3e839099a09bc40e9b53858cada8804de6c702
Ilona Bednarska <ilona.lewandowska@gmail.com>
2019-07-27 15:37:00 +0200

    pętla for, obliczanie średniej

--- testy/Program.cs
+++ testy/Program.cs
@@ -11,20 +11,49 @@ namespace testy
         static void Main(string[] args)
         {
 
-            string imie = "ilona";
+
             int i;
-
-            Console.WriteLine(imie);
-            Console.WriteLine();
-            Console.WriteLine("Początek pętli for");
-            Console.WriteLine("------------------");
-
+            
             for (i=0; i<=10; i++)
             {
-                Console.WriteLine(imie);
+                Console.WriteLine();
+                Console.Write("Podaj imię ucznia: ");
+                string imie = Console.ReadLine();
+
+                Console.Write("Podaj nazwisko ucznia: ");
+                string nazwisko = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.Write("Podaj średnią ucznia: ");
+                double srednia;
+                bool result = double.TryParse(Console.ReadLine(), out srednia);
+
+
+                if (srednia >= 4.75)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write(imie + " " + nazwisko + " " + "świadectwo z wyróżnieniem");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+
+                }
+
+                else if (result == false)
+                {
+                    Console.WriteLine("Podałeś niewłaściwą formę średniej");
+                }
+
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(imie + " " + nazwisko + " - świadectwo bez wyróżnienia");
+                    
+                }
+
             }
 
-           
+
             Console.ReadLine();
         }
     }
