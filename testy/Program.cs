47c06760bb9bb6e701572992cacfc4105db8f701
Ilona Bednarska <ilona.lewandowska@gmail.com>
2019-07-27 15:02:25 +0200

    prosta pętla for

--- testy/Program.cs
+++ testy/Program.cs
@@ -10,32 +10,18 @@ namespace testy
     {
         static void Main(string[] args)
         {
-            Console.Write("Podaj imię ucznia: ");
-            string imie = Console.ReadLine();
 
-            Console.Write("Podaj nazwisko ucznia: ");
-            string nazwisko = Console.ReadLine();
+            string imie = "ilona";
+            int i;
 
+            Console.WriteLine(imie);
             Console.WriteLine();
+            Console.WriteLine("Początek pętli for");
+            Console.WriteLine("------------------");
 
-            Console.Write("Podaj średnią ucznia: ");
-            double srednia;
-            bool result = double.TryParse(Console.ReadLine(), out srednia);
-
-            if (srednia >= 4.75)
+            for (i=0; i<=10; i++)
             {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.Write(imie + " " + nazwisko + " " + "świadectwo z wyróżnieniem");
-            }
-
-            else if (result == false)
-            {
-                Console.WriteLine("Podałeś niewłaściwą formę średniej");
-            }
-
-            else
-            {
-                Console.WriteLine(imie + " " + nazwisko + " - świadectwo bez wyróżnienia");
+                Console.WriteLine(imie);
             }
 
            
